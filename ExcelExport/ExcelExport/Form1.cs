8fffb7351a36d94d1399173a2247ec54c62e4f7d
marton.kolonics <marton.kolonics@stud.uni-corvinus.hu>
2022-10-06 23:14:51 +0200

    9.1 kesz

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -122,6 +122,8 @@ namespace ExcelExport
             xlSheet.get_Range(
                         GetCell(2, 1),
                         GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+            FormatTable();
         }
 
 
@@ -142,5 +144,17 @@ namespace ExcelExport
             return ExcelCoordinate;
         }
         
+        void FormatTable()
+        {
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, 9)); //9 helyett headers.length kellene
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+        }
+
     }
 }
